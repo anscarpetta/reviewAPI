e111f4fbe0b04bd5a81df2006774a3d9aa055fea
Angelo Scarpetta <angeloscarpetta2020@gmail.com>
2025-04-25 20:11:56 -0300

    Adicionando filtragem basica por Setor, Paginacao, Ordenacao baseada na Nota da Review.

--- src/main/java/br/com/teste/review/api/ReviewApi.java
+++ src/main/java/br/com/teste/review/api/ReviewApi.java
@@ -4,9 +4,12 @@ package br.com.teste.review.api;
 import br.com.teste.review.entity.Review;
 import br.com.teste.review.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -20,8 +23,26 @@ public class ReviewApi {
   }
 
   @GetMapping("/admin/reviews")
-  public List<Review> listarReviews(){
-    return reviewRepository.findAll();
+  public Map<String, Object> listarReviews(
+          @RequestParam(required = false, name = "setor") String setor,
+          Pageable pageable
+  ) {
+    
+    Page<Review> page = !setor.isEmpty() ?reviewRepository.findBySetor(setor, pageable): reviewRepository.findAll(pageable);
+
+    Map<String, Object> resposta = new HashMap<>();
+    resposta.put("reviews", page.getContent());
+    resposta.put("paginaAtual", page.getNumber());
+    resposta.put("paginasTotais", page.getTotalPages());
+    resposta.put("quantidadeTotal", page.getTotalElements());
+
+    return resposta;
+  }
+
+  @GetMapping("/admin/review")
+  public Review listarReview(@RequestParam String id){
+    return reviewRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Review não encontrada com ID: " + id));
   }
 
   @PostMapping("cliente/review")
--- src/main/java/br/com/teste/review/repository/ReviewRepository.java
+++ src/main/java/br/com/teste/review/repository/ReviewRepository.java
@@ -1,6 +1,12 @@
 package br.com.teste.review.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import br.com.teste.review.entity.Review;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ReviewRepository extends MongoRepository<Review, String> { }
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
+  Page<Review> findBySetor(String setor, Pageable pageable);
+
+}
